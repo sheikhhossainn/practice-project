67d99cc1303a633eca218670b4b81db02685b5aa
sheikhhossainn <skhossain799@gmail.com>
2026-05-05 23:14:22 +0600

    Task 1

---
+++ login.js
@@ -0,0 +1,46 @@
+let users = [
+    {
+        id: 1,
+        name: "Shahi",
+        password: "s1234"
+    },
+    {
+        id: 2,
+        name: "Humaira",
+        password: "h1234"
+    },
+    {
+        id: 3,
+        name: "Usha",
+        password: "u1234"
+    }
+]
+
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const submit = document.getElementById('submit-btn')
+
+submit.addEventListener('click', () =>{
+
+    event.preventDefault();
+
+    const inputName = username.value;
+    const inputPassword = password.value;
+    searchUser(inputName,inputPassword)
+    
+    console.log()
+})
+
+const searchUser = (iusername, ipassword) => {
+
+    const matchedUser = users.find(u => u.name === iusername && u.password === ipassword);
+
+    if(!matchedUser){
+        alert("Wrong credentials!😒")
+        console.log("Wrong Credentials!")
+    }
+    else{
+        alert(`Welcome back ${iusername}😊`)
+        console.log(`Welcome back ${iusername}`)
+    }
+}
